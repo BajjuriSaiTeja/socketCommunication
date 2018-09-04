c18a259aebb36964358d212fbf50ae21a71bd75c
Bajjuri Sai Teja <bsaiteja1996@gmail.com>
2018-09-04 09:43:24 +0530

    websocket

---
+++ src/main/java/com/stackroute/socket/domain/Message.java
@@ -0,0 +1,32 @@
+package com.stackroute.socket.domain;
+
+public class Message {
+
+	private String from;
+	private String text;
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Message(String from, String text) {
+		this.from = from;
+		this.text = text;
+	}
+
+	public Message() {
+	}
+
+}
